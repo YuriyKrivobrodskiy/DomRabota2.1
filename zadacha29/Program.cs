28a9210ec2acf7a02bb86b90f90b797220b0010d
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-20 17:08:23 +0300

    zadacha29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = new int[8];
+int[] FillArray()
+{
+    int Length = array.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        array[index] = new Random().Next(0, 99);
+        index++;
+    }
+    return array;
+}
+
+System.Console.WriteLine(string.Join(" ", FillArray()));
