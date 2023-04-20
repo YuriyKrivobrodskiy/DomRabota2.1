ea5758793c24bfa70751181d23bdef7c3772ad6e
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-20 14:44:50 +0300

    zadacha25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,36 @@
+﻿// задача 25: цикл, который принимает на вход два числа (А и В) и 
+// возводит число А в натуральную степень В.
+int Prpompt (string msg)
+{
+    System.Console.Write(msg);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Produkt(int a, int b)
+{
+    int produkt = 1;
+    for (int i = 0; i < b; i++)
+    {
+      produkt *= a;
+    }
+    return produkt;
+}
+
+bool CheckingB(int b)
+{
+    if (b < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int a = Prpompt("Введите число: ");
+int b = Prpompt("Введите степень: ");
+if (CheckingB(b))
+{
+    System.Console.WriteLine($"Число {a} в степени  {b} равно {Produkt(a, b) }");
+}
